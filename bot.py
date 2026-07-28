9192fb7a9b579fe592e273be4edef9752571cc95
biredangiadida-cell <biredangiadida@gmail.com>
2026-07-28 23:37:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,4 +53,70 @@ def save_members(data):
         json.dump(data, file, indent=4)
 
 
-members = load_members()
+members = load_members()# ================= START =================
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "📝 Galmaa'i",
+                callback_data="register"
+            ),
+            InlineKeyboardButton(
+                "📚 Koorsoota",
+                callback_data="courses"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "💳 Kaffaltii",
+                callback_data="payment"
+            ),
+            InlineKeyboardButton(
+                "📤 Receipt",
+                callback_data="receipt"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "👤 Account Koo",
+                callback_data="account"
+            ),
+            InlineKeyboardButton(
+                "🌐 Website",
+                callback_data="website"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "📢 Beeksisa",
+                callback_data="news"
+            ),
+            InlineKeyboardButton(
+                "📞 Nu Qunnamaa",
+                callback_data="contact"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "❓ Gargaarsa",
+                callback_data="help"
+            ),
+            InlineKeyboardButton(
+                "ℹ️ Waa'ee ELBS",
+                callback_data="about"
+            )
+        ]
+    ]
+
+
+    await update.message.reply_text(
+        "🎓 *ELBS*\n"
+        "Everlasting Love Bible School\n\n"
+        "Baga nagaan dhuftan!\n\n"
+        "📖 Barnoota Macaafa Qulqulluu online.\n\n"
+        "👇 Tajaajila barbaaddan keessaa tokko filadhaa.",
+        reply_markup=InlineKeyboardMarkup(keyboard),
+        parse_mode="Markdown"
+    )
